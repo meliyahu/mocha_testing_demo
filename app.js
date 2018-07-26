0a78b4273c6f129d079d57f423580d9471913807
Mosheh EliYahu <mosheh.eliyahu@adelaide.edu.au>
2018-07-26 15:01:30 +0930

    all in

---
+++ app.js
@@ -0,0 +1,11 @@
+module.exports =  {
+    sayHello: () => {
+        return 'hello';
+    },
+    sayGoodBye: () => {
+        return 'Good Bye';
+    },
+    addNumbers: (v1, v2) => {
+        return v1 + v2;
+    }
+}
